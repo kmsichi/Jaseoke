19c6f33ec73ab02a04bb36a5b2aec8c97887b2e0
SichiKim <sky06sky@naver.com>
2025-07-06 18:41:13 +0900

    시간 표기 수정

--- util/SecondsToString.js
+++ util/SecondsToString.js
@@ -1,9 +1,9 @@
 function secondsToString(t) {
     let hour = undefined;
     if (t/3600 > 1)
-        hour = (t/3600) > 10 ? (t/3600) : '0'+(t/3600);
-    let min = (t%3600/60) > 10 ? (t%3600/60) : '0'+(t%3600/60);
-    let sec = (t%60) > 10 ? (t%60) : '0'+(t%60);
+        hour = (t/3600) > 10 ? parseInt(t/3600) : '0'+parseInt(t/3600);
+    let min = (t%3600/60) > 10 ? parseInt(t%3600/60) : '0'+parseInt(t%3600/60);
+    let sec = (t%60) > 10 ? parseInt(t%60) : '0'+parseInt(t%60);
 
     if (hour == undefined) 
         return `${min}:${sec}`;
